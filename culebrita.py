6b4f1d93554c6f311dd41fa6f0328f322fd8cbc2
JeanPierre2404 <jean05343@gmail.com>
2023-02-12 00:42:07 -0500

    Update culebrita.py
    V1.2

--- culebrita.py
+++ culebrita.py
@@ -1,5 +1,11 @@
 import turtle
 
 ventana = turtle.Screen()#crear ventana
+ventana.title('Juego Skanek | Amadeus')#colocando titulo
+ventana.bgcolor('black')#colocando color
+ventana.setup(width=600,height=600)#dimensiones de la ventana
+ventana.tracer(0)#animaciones placenteras xd
+
+
 
 ventana.exitonclick()#evitar que se apague la pantalla solito
